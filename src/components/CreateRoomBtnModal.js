529f481b09d3e1f0d52bcd2e63d2e4548af24fe2
Shashwat Sagar <shashwatsagar19@gmail.com>
2022-01-23 12:52:32 +0530

    creation of chatroom window is initialized

---
+++ src/components/CreateRoomBtnModal.js
@@ -0,0 +1,80 @@
+import React, { useCallback, useRef, useState } from 'react';
+import {
+  Button,
+  ControlLabel,
+  Form,
+  FormControl,
+  FormGroup,
+  Icon,
+  Modal,
+  Schema,
+} from 'rsuite';
+import { useModalState } from '../misc/custom-hooks';
+
+const { StringType } = Schema.Types;
+
+const model = Schema.Model({
+    name: StringType().isRequired('Chat name is required!'),
+    description: StringType().isRequired('Description is required!'),
+})
+
+const INITIAL_FORM = {
+    name: '',
+    description: ''
+}
+
+const CreateRoomBtnModal = () => {
+  const { isOpen, open, close } = useModalState();
+
+  const [formValue, setFormValue] = useState(INITIAL_FORM);
+  const [isLoading, setIsLoading] = useState(false);
+  const formRef = useRef();
+
+  const onFormChange = useCallback(value => {
+      setFormValue(value);
+  }, []);
+
+const onSubmit = async () => {
+
+    if(!formRef.current.check()) {
+        return;
+    }
+}
+
+  return (
+    <div className="mt-1">
+      <Button block color="green" onClick={open}>
+        <Icon icon="creative" /> Create New chat room
+      </Button>
+      <Modal show={isOpen} onHide={close}>
+        <Modal.Header>
+          <Modal.Title>New chat room</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form fluid onChange={onFormChange} formValue={formValue} model={model} ref={formRef}>
+            <FormGroup>
+              <ControlLabel>Room Name</ControlLabel>
+              <FormControl name="name" placeholder="Enter chat room name..." />
+            </FormGroup>
+            <FormGroup>
+              <ControlLabel>Description</ControlLabel>
+              <FormControl
+                componentClass="textarea"
+                rows={5}
+                name="description"
+                placeholder="Enter room description..."
+              />
+            </FormGroup>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button block appearance="primary">
+            Create new chat room
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
+
+export default CreateRoomBtnModal;
